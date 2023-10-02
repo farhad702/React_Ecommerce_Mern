75acfed6bfede0e765eb3a08995199ea17a2ff04
Farhad Noor <farhad_702@hotmail.com>
2023-10-02 16:29:15 +0600

    Api_Login_User_Assign_Token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -25,3 +25,34 @@ exports.registerUser = catchAsyncErrors(async(req,res,next) =>{
         token
     })
 })
+
+// Login user => /api/v1/login
+exports.loginUser = catchAsyncErrors(async(req,res,next)=>{
+    const { email, password } = req.body;
+
+    // Check if email and password is entered by user
+    if(!email || !password){
+        return next(new ErrorHandler('Please enter email & password',400))
+    }
+
+    // Finding user in database
+    const user = await User.findOne({ email }).select('+password')
+
+    if(!user){
+        return next(new ErrorHandler('Invalid Email or Password',401));
+    }
+
+    //Check if password is correct or not
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if(!isPasswordMatched){
+        return next(new ErrorHandler('Invalid Email or password', 401));
+    }
+
+    const token = user.getJwtToken();
+
+    res.status(200).json({
+        success:true,
+        token
+    })
+})
