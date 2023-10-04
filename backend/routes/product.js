0bd5468851ecf53c3dc420a53e201011c2999f50
Farhad Noor <farhad_702@hotmail.com>
2023-10-04 11:42:31 +0600

    Create_api_Logout_User

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const {getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct} = require ('../controllers/productController');
 
 const { isAuthenticateUser } = require('../middlewares/auth');
-router.route('/products').get(isAuthenticateUser, getProducts);
+router.route('/products').get( getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/product/new').post(newProduct);
+router.route('/product/new').post( isAuthenticateUser,newProduct);
 
-router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct);
+router.route('/admin/product/:id').put(isAuthenticateUser, updateProduct).delete( isAuthenticateUser, deleteProduct);
 module.exports = router;
