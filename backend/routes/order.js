4ab21356bcc2f70044aeb3e47a61e8c087e16e08
Farhad Noor <farhad_702@hotmail.com>
2023-10-15 16:23:04 +0600

    Get_Single_Order_Logged_In_User_Order

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router();
 
-const { newOrder } = require('../controllers/orderController')
+const { newOrder, getSingleOrder, myOrders } = require('../controllers/orderController')
 
 const { isAuthenticateUser , authorizeRoles } = require('../middlewares/auth')
 
 router.route('/order/new').post( isAuthenticateUser, newOrder);
 
+router.route('/order/:id').get(isAuthenticateUser, getSingleOrder);
+router.route('/orders/me').get(isAuthenticateUser, myOrders);
 module.exports = router;
