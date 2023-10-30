f8284034a7596fc646f875548967bece3e6e9772
Farhad Noor <farhad_702@hotmail.com>
2023-10-30 15:57:57 +0600

    user_review_add_delete_review

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const router = express.Router();
 
-const {getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct} = require ('../controllers/productController');
+const {
+    getProducts, 
+    newProduct, 
+    getSingleProduct, 
+    updateProduct, 
+    deleteProduct, 
+    createProductReview,
+    getProductReviews,
+    deleteReview
+} = require ('../controllers/productController');
 
 const { isAuthenticateUser, authorizeRoles } = require('../middlewares/auth');
 router.route('/products').get( getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/admin/product/new').post( isAuthenticateUser,authorizeRoles('admin'), newProduct);
+router.route('/admin/product/new').post( isAuthenticateUser,authorizeRoles('admin'), 
+newProduct);
 
-router.route('/admin/product/:id').put(isAuthenticateUser, authorizeRoles('admin'), updateProduct).delete( isAuthenticateUser, authorizeRoles('admin'), deleteProduct);
-module.exports = router;
+router.route('/admin/product/:id')
+.put(isAuthenticateUser, authorizeRoles('admin'), updateProduct)
+.delete( isAuthenticateUser, authorizeRoles('admin'), deleteProduct);
+
+router.route('/review').put(isAuthenticateUser, createProductReview)
+router.route('/reviews').get(isAuthenticateUser, getProductReviews)
+router.route('/reviews').delete(isAuthenticateUser, deleteReview)
+module.exports = router; 
